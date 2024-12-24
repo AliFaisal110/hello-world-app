d55510d26a82b57b8b88e7b1105ead5f203cbd03
Your Name <you@example.com>
2024-12-24 19:48:27 +0500

    Trigger pipeline rerun

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,16 +15,22 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
-variable "public_subnet_cidr" {
-  description = "CIDR block for the public subnet"
+variable "public_subnet_cidr_a" {
+  description = "CIDR block for public subnet A"
   type        = string
   default     = "10.0.1.0/24"
+}
+
+variable "public_subnet_cidr_b" {
+  description = "CIDR block for public subnet B"
+  type        = string
+  default     = "10.0.2.0/24"
 }
 
 variable "private_subnet_cidr" {
   description = "CIDR block for the private subnet"
   type        = string
-  default     = "10.0.2.0/24"
+  default     = "10.0.3.0/24"
 }
 
 variable "instance_type" {
