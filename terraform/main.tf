d8be3906c0a284640393870fb0a29060446a531a
Your Name <you@example.com>
2024-12-24 23:11:41 +0500

    Final

--- terraform/main.tf
+++ terraform/main.tf
@@ -145,6 +145,7 @@ resource "aws_lb_target_group" "app_target_group" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+  target_type = "ip"  # Change to "ip" for ECS Fargate
 }
 
 resource "aws_lb_listener" "app_listener" {
@@ -156,4 +157,10 @@ resource "aws_lb_listener" "app_listener" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.app_target_group.arn
   }
+}
+
+resource "aws_lb_target_group_attachment" "target_group_attachment" {
+  target_group_arn = aws_lb_target_group.app_target_group.arn
+  target_id        = aws_ecs_service.service.id  # Attach the ECS service to the target group
+  port             = 80
 }
